dfd8353f0a5b005ffdfa32623d999aadc44436e4
joanrig <github email address>
2019-05-08 15:30:03 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+    resources :preferences, only: [:index]
+  end
+
+  resources :artists, except: [:destroy] do
+    resources :songs, only: [:index, :show]
+  end
+
+  resources :songs, except: [:destroy]
 end
